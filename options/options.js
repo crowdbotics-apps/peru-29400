e34503428711037a9db1dcb8eb54599f7e5a4bbc
Crowdbotics <team@crowdbotics.com>
2021-08-03 14:42:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "peru_29400Identifier",
+  url: "https://peru_29400Identifier.botics.co",
+  api: "https://peru_29400Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
